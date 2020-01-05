6ad1558726eb4bc4d59b1730bc0787c101f738c8
Oldahir Gomez <oldahir_gomez25@hotmail.com>
2020-01-05 02:31:51 -0600

    Chat finzlizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -12,12 +12,16 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(res) {
-        console.log('Usuarios conectados', res);
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -29,8 +33,8 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -38,18 +42,20 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
-// Mensaje privado
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado: ', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
